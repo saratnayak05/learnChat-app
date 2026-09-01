166bb1dfe16dac5f5feb4e695c2cec036f173c5c
Sarat Nayak <152516593+saratnayak05@users.noreply.github.com>
2026-09-01 22:45:49 +0530

    Add VideoIcon to CallButton component

--- client/src/components/CallButton.jsx
+++ client/src/components/CallButton.jsx
@@ -5,6 +5,7 @@ function CallButton({ handleVideoCall }) {
     <div className="p-3 border-b flex items-center justify-end max-w-7xl mx-auto w-full absolute top-0">
       <button onClick={handleVideoCall} className="btn btn-success btn-sm text-white">
         <VideoIcon className="size-6" />
+        
       </button>
     </div>
   );
